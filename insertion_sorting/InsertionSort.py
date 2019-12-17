9b0f3454bce3a8cb121191e290843180c0661502
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:33:16 +0530

    [Shalinee Bhawnani].Modified insertion_sorting

--- insertion_sorting/InsertionSort.py
+++ insertion_sorting/InsertionSort.py
@@ -3,14 +3,22 @@
 # @file  :InsertionSort.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+# storing array element
 arr = [122, 131, 153, 25, 64]
+
+#creating metod to take array
 def insertionsort(arr):
     for i in range(1,len(arr)):
         key=arr[i]
         j=i-1
+
+        #swaping element
         while j>=0 and key<arr[j]:
             arr[j+1]=arr[j]
             j-=1
         arr[j+1]=key
+#calling method
 insertionsort(arr)
+
+#print sorted array
 print (arr)
