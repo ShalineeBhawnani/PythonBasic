e812bb0e8954c0697b4d5e77b7c2c42764f84dcc
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 12:45:26 +0530

    [Shalinee Bhawnani]Add. added Utility Class

---
+++ List/parenthesis/Parentheses.py
@@ -0,0 +1,19 @@
+# ******************************************************************************************************************
+# @purpose :Demonstrate Parentheses.
+# @file  :Parentheses.py
+# @author :ShalineeBhawnani
+# *******************************************************************************************************************str = input("enter the string")
+
+def check(str):
+    stack = []
+    for i in str:
+        if i =='(' or i=='{' or i== '[':
+            stack.append(i)
+        elif i ==')' or i=='}' or i== ']':
+            stack.pop()
+    if len(stack)==0:
+        print(True)
+    else:
+        print(False)
+
+check(str)
