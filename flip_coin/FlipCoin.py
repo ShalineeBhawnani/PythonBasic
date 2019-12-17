75f4fdee23fb8751ef684462e145ab0eca952dad
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:02:17 +0530

    [Shalinee Bhawanani].Modified flip_coin

--- flip_coin/FlipCoin.py
+++ flip_coin/FlipCoin.py
@@ -3,11 +3,19 @@
 # @file  :FlipCoin.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+
+#import random function
 import random
+
+#creating method to toss coin
 def flipcoin():
+
+    #initialize head & tail 0
     headcount = 0
     tailcount = 0
     count = 0
+
+    #checking count value
     while count <100:
         coin= random.randint(1,2)
 
@@ -19,6 +27,10 @@ def flipcoin():
             print("tail\n")
             tailcount+=1
             count+=1
+
+    # return head & tail count
     return headcount,tailcount
+    
+# calling flipcoin() & printing 
 print(flipcoin())
 
