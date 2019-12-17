e5d8c773c30937c50a4f13aec2b9a03435ba06e4
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:32:05 +0530

    [Shalinee Bhawnani].Modified gambler

--- gambler/Gambler.py
+++ gambler/Gambler.py
@@ -3,16 +3,26 @@
 # @file  :Gambler.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+
+#import random function 
 import random
+
+#taking value of stake,goal & trails
 def gambler(stake,goal,trails):
+
+    # Run trialCount experiments that start with stake and terminate on
+    # 0 or goal.
     bets=0
     win=0
     loss=0
     for i in range(0,51):
         cash=stake
         while(cash>0 and cash<goal):
+
+            # Simulate one bet.
             bets+=1
 
+            #storing random number
             num= random.randint(1,2)
             if num==1:
                 cash+=1
@@ -23,6 +33,8 @@ def gambler(stake,goal,trails):
                 win+=1
             else:
                 loss+=1
+                
+        #printing number of bets
         print("number of bet", bets)
         return win,loss
 
