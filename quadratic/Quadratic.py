7fe961d40f7cd10b95c41dfabd210b8d913e60fb
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:35:50 +0530

    [Shalinee Bhawnani].Modified quadratic

--- quadratic/Quadratic.py
+++ quadratic/Quadratic.py
@@ -3,15 +3,22 @@
 # @file  :Quadratic.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+
+#import math funtion
 import math
 a = 3
 b = 19
 c = 8
+
+# creating method for getting delta
 def quadratic(a,b,c):
     delta=b*b-4*a*c
 
+    # deffinig root
     if delta>0:
         root1=(-b+math.sqrt(delta))/(2*a)
         root2=(-b-math.sqrt(delta))/(2*a)
+    
+    #returning root value
     return [root1,root2]
 
--- quadratic/QuadraticTest.py
+++ quadratic/QuadraticTest.py
@@ -6,12 +6,15 @@
 import unittest
 from Quadratic import quadratic
 
-
+#creating test case
 class MyTestCase(unittest.TestCase):
     def test_Quadratic(self):
+
+        #calling method & storing result
         result=quadratic(3,19,8)
+        #comparing result with expected data
         self.assertListEqual(result,[-0.453529900650049,-5.879803432683285])
 
-
+#to check whether the module is being imported or not
 if __name__ == '__main__':
     unittest.main()
