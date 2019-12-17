bd65cd2331235eca8f3797596d14a9c79177e515
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 18:57:44 +0530

    [Shalinee Bhawnani].Modified binarysearch

--- binarysearch/BinarySearch.py
+++ binarysearch/BinarySearch.py
@@ -3,10 +3,14 @@
 # @file  :BinarySearch.py
 # @author :ShalineeBhawnani
 #*******************************************************************************************************************
+
+# taking input from user
 #arr = [ 2, 3, 4, 10, 40 ]
 arr = ['aa','bb','cc','dd','ee','ff','gg','hh']
 #n=int(input("enter number from given index:"))
 n=input("enter string for search:")
+
+# creating method to check binary number
 def binary(arr,f,l,n):
     if f<=l:
         mid=(l+f)//2
@@ -19,6 +23,8 @@ def binary(arr,f,l,n):
             f = mid + 1
             return f
     return -1
+
+# storing result 
 result=binary(arr,0,len(arr)-1,n)
 if result != -1:
     print("Element is present at index", result)
