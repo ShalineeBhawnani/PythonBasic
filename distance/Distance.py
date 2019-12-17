200ad2d697557519820de629cc27b19734d56f3b
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:00:07 +0530

    [Shalinee Bhawanani].Modified distance

--- distance/Distance.py
+++ distance/Distance.py
@@ -3,9 +3,14 @@
 # @file  :Distance.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+#importing math function
 import math
+
+#creating method to calculate distance between two point
 def calculateDistance(x1,y1,x2,y2):
     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+    #printing distance
     print(dist)
     return dist
 
--- distance/DistanceTest.py
+++ distance/DistanceTest.py
@@ -7,15 +7,26 @@ import unittest
 import math
 from Distance import calculateDistance
 
+#creating class for unit test
 class MyTestCase(unittest.TestCase):
     def test_distance(self):
+
+        #calling method & storing result
         result = calculateDistance(1,2,3,4)
+
+        #comparing result with expected data
         self.assertEqual(result,2.8284271247461903)
 
+        #calling method & storing result
         result = calculateDistance(5,2,3,5)
+
+        #comparing result with expected data
         self.assertEqual(result,3.605551275463989)
 
+        #calling method & storing result
         result = calculateDistance(4,5,2,4)
+
+        #comparing result with expected data
         self.assertEqual(result,2.23606797749979)
 
 
