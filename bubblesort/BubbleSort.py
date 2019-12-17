5bd4826a158a7cc590c52ff37d35178ecbcc4577
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 18:58:43 +0530

    [Shalinee Bhawnani].Modified bubblesort

--- bubblesort/BubbleSort.py
+++ bubblesort/BubbleSort.py
@@ -3,11 +3,21 @@
 # @file  :BubbleSort.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+
+#creating method for sorting array
 def bubbleSort(arr):
+
+    #checking each & every element 
     for i in range(len(arr)):
+
+        #comparing elements
         for j in range(0,len(arr)-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
 arr=[12,33,56,87,123,90,33,55,12]
+
+#calling sorting method
 bubbleSort(arr)
+
+#printing array
 print(arr)
