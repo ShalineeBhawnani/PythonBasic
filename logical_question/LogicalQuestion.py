04ce00ee99b7c967c3483d45eeb2e8045acac474
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:34:55 +0530

    [Shalinee Bhawnani].Modified logical_question

--- logical_question/LogicalQuestion.py
+++ logical_question/LogicalQuestion.py
@@ -1,8 +1,13 @@
+#taking input from user
 num = int(input("Enter a number: "))
+
+#creating method to take number
 def computer_guess(num):
     low = 1
     high = 100
     guess = 50
+
+    #giving range
     if num not in range(0,101):
         print("Must be in range [1, 100]")
         return
@@ -14,5 +19,6 @@ def computer_guess(num):
         elif guess < num:
             low = guess + 1
 
+    #printing number
     print("The computer guessed", guess, "and it was correct!")
 computer_guess(num)
