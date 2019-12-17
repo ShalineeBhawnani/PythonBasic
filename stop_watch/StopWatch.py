c9c47d041263517f8f392a1acd520641a940136f
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:36:20 +0530

    [Shalinee Bhawnani].Modified stop_watch

--- stop_watch/StopWatch.py
+++ stop_watch/StopWatch.py
@@ -3,17 +3,29 @@
 # @file  :StioWatch.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+#importing time
 import time
+
+# creating method to check time difference
 def stopwatch():
     startTimer=0
     stopTimer=0
+
+    #taking starting time
     time1=input("Do u want to start?")
     time_start=time.time()
+
+    #taking stop time 
     time2= input("Do u want to stop?")
     time_stop=time.time()
+
+    #finding difference between start & ending time
     mid=time_stop-time_start
+
+    #printing difference
     print(mid)
     return mid
 
+#calling method for execution
 stopwatch()
 
