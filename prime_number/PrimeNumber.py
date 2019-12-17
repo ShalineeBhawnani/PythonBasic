3ed5218e3d9114bd6f263b2beba0a7c8148535a3
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:35:23 +0530

    [Shalinee Bhawnani].Modified prime_number

--- prime_number/PrimeNumber.py
+++ prime_number/PrimeNumber.py
@@ -1,5 +1,7 @@
 start = 0
 end = 1000
+
+#method to check prime number
 for n in range(start, end + 1):
 
     if n > 1:
@@ -7,4 +9,5 @@ for n in range(start, end + 1):
             if (n % i) == 0:
                 break
         else:
+            #print prime number
             print(n)
