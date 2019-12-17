b8c2cc0ff8deedb18ea5ac5bb4bdfaaf537650c2
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 17:03:07 +0530

    [Shalinee Bhawnani].Modified Utility

--- List/Utility/util.py
+++ List/Utility/util.py
@@ -1,35 +1,47 @@
+import os
+
+# A structure to represent a queue  
 class Queue:
 
+    # default constructor to ininialize data
     def __init__(self):
         self.queue = []
 
+    # method for add an item to the right side of the list
     def enqueue(self, item):
         self.queue.append(item)
 
+    # method for removing data
     def dequeue(self):
         if len(self.queue) < 1:
             return None
         return self.queue.pop(0)
 
+    # count the number of items with a certain value
     def size(self):
         return len(self.queue)
 
+    # print the queue
     def printQueue(self):
       return self.queue
 
-
-
+# creating Class
 class Node:
+
+    # constructor to initiate this object
     def __init__(self,data):
         self.data = data
         self.next = None
 
+# creating class
 class PrimeQueue:
+
+    # default constructor
     def __init__(self):
         self.first =  None
 
 
-    #insert at last
+    #insert data at last 
     def pqueue(self,value):
         newnode = Node(value)
         if (self.first == None):
