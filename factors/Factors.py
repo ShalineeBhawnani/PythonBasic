93bbdc9154361ae28064ea93b3d833cd5859477d
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-17 19:01:10 +0530

    [Shalinee Bhawanani].Modified factors

--- factors/Factors.py
+++ factors/Factors.py
@@ -3,14 +3,22 @@
 # @file  :Factors.py
 # @author :ShalineeBhawnani
 # *******************************************************************************************************************
+
+#creating method to check prime factors
 def prime_factors(n):
    i=2
+
+   #creating empty list
    factor_list=[]
+
+   #taking given condition
    while i * i < n:
        while n % i == 0:
            factor_list.append(i)
            n = n / i
        i = i + 1
        return factor_list
+
+#printing prim factors
 print(prime_factors(80))
 
