915f4bd759177557eb86647560bc325752d17245
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-18 17:18:06 +0530

    [Shalinee Bhawnani].Modified json

--- json/Inventory.py
+++ json/Inventory.py
@@ -1,1 +1,10 @@
 import json
+
+with open('json_file.json') as f:
+    data= json.load(f)
+for Rice in data['Rice']:
+    print(Rice)
+for Pulses in data['Pulses']:
+    print(Pulses)
+for Wheats in data['Wheats']:
+    print(Wheats)
