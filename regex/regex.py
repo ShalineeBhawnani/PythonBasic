cd8149d531988ad9c67d028ae572a3d8e5193590
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-19 15:57:18 +0530

    [Shalinee Bhawnani]. Modified regex

--- regex/regex.py
+++ regex/regex.py
@@ -1,11 +1,31 @@
+#******************************************************************************************************************
+# @purpose :Evaluate the Iventry object using JSON.
+# @file  :inventory.py
+# @author :ShalineeBhawnani
+#*******************************************************************************************************************
+
+#importing regexp module
 import re
-with open ('file.txt', 'r') as f:
-    text = f.read()
-    print(text)
-result = re.sub(r"[<]{2}[a-zA-Z]{4}[>]{2}", "<<Shalu>>", text)
-result = re.sub(r"[<]{2}[a-zA-Z]{8}[>]{2}", "<<Shalu Bhawnani>>", result)
-result = re.sub(r"(x){10}", "4567586948", result)
-result = re.sub(r"XX/XX/XXXX", "01/01/2016", result)
-file1 = open("file.txt","w")
-file1.write(result) 
-print(result)
+class regex:
+    #method for reading txt file
+    def check(self):
+        with open ('file.txt', 'r') as f:
+            text = f.read()
+            print(text)
+        try:
+            #pattern matching
+            result = re.sub(r"[<]{2}[a-zA-Z]{4}[>]{2}", "<<Shalu>>", text)
+            result = re.sub(r"[<]{2}[a-zA-Z]{8}[>]{2}", "<<Shalu Bhawnani>>", result)
+            result = re.sub(r"(x){10}", "4567586948", result)
+            result = re.sub(r"XX/XX/XXXX", "01/01/2016", result)
+            file1 = open("file.txt","w")
+            file1.write(result) 
+            print(result)
+        except:
+            #printing error
+            print("An exception occurred")
+ 
+#creating object
+obj=regex()
+#calling method 
+obj.check()
