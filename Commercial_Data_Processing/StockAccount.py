43111aec372502fa353ab8c1e6a434d4e2e7ef02
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-23 19:40:50 +0530

    [Shalinee Bhawnani].Modified Commercial_Data_Processing

--- Commercial_Data_Processing/StockAccount.py
+++ Commercial_Data_Processing/StockAccount.py
@@ -17,56 +17,87 @@ class StockAccount:
         data3=data_key["Products"]
         entry1=0
 
-        #taking input from user
         type=int(input("\nType:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing"))
         if type==0:
-            print("a")
-            entry1 = data2[0]
-            entry1=entry1["amount"]
-            print(entry1)
-            return entry1
-       
+            for data in data_key["Shares"]:
+                print("hello") 
+                if data['share'] == "Production":
+                    print("heeello") 
+                    data['amount'] -= 50
+                    print(data_key)
+                    with open('company_shares.json', '+w') as json_file:
+                        json.dump(data_key, json_file)
         if type==1:
-           entry1 = data2[1]
-           entry1=entry1["amount"]
-           return entry1
-       
+            for data in data_key["Shares"]:
+                print("hello")
+                if data['share'] == "Sales":
+                    print("heeello") 
+                    data['amount'] -= 50
+                    print(data_key)
+                    with open('company_shares.json', '+w') as json_file:
+                        json.dump(data_key, json_file)
         if type==2:
-            entry1 = data2[2]
-            entry1=entry1["amount"]
-            return entry1
-        else: 
-            print ("wrong chooise") 
-        return entry1
+            for data in data_key["Shares"]:
+                print("hello")
+                if data['share'] == "Marketing":
+                    print("heeello") 
+                    data['amount'] -= 50
+                    print(data_key)
+                    with open('company_shares.json', '+w') as json_file:
+                        json.dump(data_key, json_file)
 
-    # def updateJson(self,data2):
-    #     result=[sub["amount"] for sub in data2 ]                                                                                        
-    #     input_amount=int(input("enter amount"))
-    #     for i in result:
-    #         list1=i- input_amount
-    #         print(list1)
-    #         return list1
-
-    #updating json data
-    def updateJson(self,entry1):
-        input_amount=int(input("enter amount"))
-        sub = entry1 - input_amount
-        print(sub)
-        return sub
+        else: ("bye")
         
+    #     #taking input from user
+    #     type=int(input("\nType:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing"))
+    #     if type==0:
+    #         print("a")
+    #         entry1 = data2[0]
+    #         entry1=entry1["amount"]
+    #         print(entry1)
+    #         return entry1
+       
+    #     if type==1:
+    #        entry1 = data2[1]
+    #        entry1=entry1["amount"]
+    #        return entry1
+       
+    #     if type==2:
+    #         entry1 = data2[2]
+    #         entry1=entry1["amount"]
+    #         return entry1
+    #     else: 
+    #         print ("wrong chooise") 
+    #     return entry1
+
+    # # def updateJson(self,data2):
+    # #     result=[sub["amount"] for sub in data2 ]                                                                                        
+    # #     input_amount=int(input("enter amount"))
+    # #     for i in result:
+    # #         list1=i- input_amount
+    # #         print(list1)
+    # #         return list1
+
+    # #updating json data
+    # def updateJson(self,entry1):
+    #     input_amount=int(input("enter amount"))
+    #     sub = entry1 - input_amount
+    #     print(sub)
+    #     return sub
+    
 #creating object
 obj1=StockAccount()
 obj2=obj1.getStockDetails()
-obj3=obj1.updateJson(obj2)
+# obj3=obj1.updateJson(obj2)
 
 if __name__ == '__main__' :
 
     inp = input(" Are having an Existing Account? (y/n):")
     if (inp == "y") or (inp == "Y"):
         filename = input("Enter the File name: ")
-        #obj1.getStockDetails()
-        with open('person.json', '+w') as json_file:
-            json.dump(obj3, json_file)
+        # # obj1.getStockDetails()
+        # with open('person.json', '+w') as json_file:
+        #     json.dump(obj3, json_file)
     else:
         f = open("newUser.json","a+")
         name = input("Enter the Name: ")
