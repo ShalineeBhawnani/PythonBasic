032da6ef37b82a25af394fdda128c39b0b3748ee
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-23 12:55:07 +0530

    [Shalinee Bhawnani].Modified  Commercial_Data_Processing

--- Commercial_Data_Processing/StockAccount.py
+++ Commercial_Data_Processing/StockAccount.py
@@ -9,16 +9,47 @@ class StockAccount:
         data1=data_key["Company"]
         data2=data_key["Shares"]
         data3=data_key["Products"]
-        return data2
+        entry1=0
+        type=input("Type:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing")
+        if type==0:
+            entry1 = data2[0]
+            entry1=entry["amount"]
+            print(entry1)
+            return entry1
+       
+        elif type==1:
+           entry1 = data2[1]
+           entry1=entry["amount"]
+           return entry1
+       
+        elif type==2:
+            entry1 = data2[2]
+            entry1=entry["amount"]
+            return entry1
+        #count = 0
+        # for name in entry:
+        #     print(name["amount"])
+        #     count+=1
+        # print(count)
+            
+        else: 
+            print ("wrong chooise") 
+        return entry1
+        
+    
 
-    def updateJson(self,data2):
-        result=[sub["amount"] for sub in data2 ]
+    # def updateJson(self,data2):
+    #     result=[sub["amount"] for sub in data2 ]
+    #     input_amount=int(input("enter amount"))
+    #     for i in result:
+    #         list1=i- input_amount
+    #         print(list1)
+    #         return list1
+    def updateJson(self,entry1):
         input_amount=int(input("enter amount"))
-        for i in result:
-            list1=i- input_amount
-            print(list1)
-            return list1
-
+        sub = entry1 - input_amount
+        print(sub)
+        return sub
 
 obj1=StockAccount()
 obj2=obj1.getStockDetails()
