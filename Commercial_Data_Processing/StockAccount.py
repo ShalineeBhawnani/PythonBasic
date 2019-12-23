69d5a664a985c691869f26d7d1527d31a177db2f
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-23 16:10:58 +0530

    [Shalinee Bhawnani].Modified Commercial_Data_Processing

--- Commercial_Data_Processing/StockAccount.py
+++ Commercial_Data_Processing/StockAccount.py
@@ -1,56 +1,60 @@
+#******************************************************************************************************************
+# @purpose :Evaluate the Stock Report using JSON.
+# @file  :Stock.py
+# @author :ShalineeBhawnani
+#*******************************************************************************************************************
 import json
 class StockAccount:
-    def getStockDetails(self):
-        #Reading JSON from a File
-        file=open("/home/bridgelabz/Videos/DS/Commercial_Data_Processing/company_shares.json","r")
 
+    #getting details from json file
+    def getStockDetails(self):
+        file=open("/home/bridgelabz/Videos/DS/Commercial_Data_Processing/company_shares.json","r")
+        #loading data from json
         data_key= json.load(file)
         print(data_key)
         data1=data_key["Company"]
         data2=data_key["Shares"]
         data3=data_key["Products"]
         entry1=0
-        type=input("Type:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing")
+
+        #taking input from user
+        type=int(input("\nType:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing"))
         if type==0:
+            print("a")
             entry1 = data2[0]
-            entry1=entry["amount"]
+            entry1=entry1["amount"]
             print(entry1)
             return entry1
        
-        elif type==1:
+        if type==1:
            entry1 = data2[1]
-           entry1=entry["amount"]
+           entry1=entry1["amount"]
            return entry1
        
-        elif type==2:
+        if type==2:
             entry1 = data2[2]
-            entry1=entry["amount"]
+            entry1=entry1["amount"]
             return entry1
-        #count = 0
-        # for name in entry:
-        #     print(name["amount"])
-        #     count+=1
-        # print(count)
-            
         else: 
             print ("wrong chooise") 
         return entry1
-        
-    
 
     # def updateJson(self,data2):
-    #     result=[sub["amount"] for sub in data2 ]
+    #     result=[sub["amount"] for sub in data2 ]                                                                                        
     #     input_amount=int(input("enter amount"))
     #     for i in result:
     #         list1=i- input_amount
     #         print(list1)
     #         return list1
+
+    #updating json data
     def updateJson(self,entry1):
         input_amount=int(input("enter amount"))
         sub = entry1 - input_amount
         print(sub)
         return sub
-
+        
+#creating object
 obj1=StockAccount()
 obj2=obj1.getStockDetails()
 obj3=obj1.updateJson(obj2)
@@ -60,8 +64,8 @@ if __name__ == '__main__' :
     inp = input(" Are having an Existing Account? (y/n):")
     if (inp == "y") or (inp == "Y"):
         filename = input("Enter the File name: ")
-        obj1.getStockDetails()
-        with open('person.json', 'w') as json_file:
+        #obj1.getStockDetails()
+        with open('person.json', '+w') as json_file:
             json.dump(obj3, json_file)
     else:
         f = open("newUser.json","a+")
