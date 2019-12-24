ecb13ae85d6d722b41affa00553e45055493df91
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-24 17:11:25 +0530

    [ShalineeBhawnani].Modified Clinique_Management

--- Clinique_Management/Clinique.py
+++ Clinique_Management/Clinique.py
@@ -3,16 +3,22 @@
 # @file  :Clinique.py
 # @author :ShalineeBhawnani
 #*******************************************************************************************************************
-import json
+try:
+    import json
+except ImportError:
+    print("import error")
 class Clinique:
     def FileOpen(self):
-        with open('json_file.json') as file:
-            data=json.load(file)
-            for p in data['Doctors']:
-                print('Name: ' + p['name'])
-                print('Gender: ' + p['gender'])
-                print('Type: ' + p['type'])
-                print("Address:" +p['address'])
-                print('ContactNumber ' +p['contactNumber'])
+        try:
+            with open('json_file.json') as file:
+                data=json.load(file)
+        except FileNotFoundError:
+            print("file not found")
+        for p in data['Doctors']:
+            print('Name: ' + p['name'])
+            print('Gender: ' + p['gender'])
+            print('Type: ' + p['type'])
+            print("Address:" +p['address'])
+            print('ContactNumber ' +p['contactNumber'])
 clinique=Clinique()
 clinique.FileOpen()
