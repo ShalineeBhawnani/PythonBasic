9f08fa60de667767f366485d007e2634196787e8
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-24 17:07:38 +0530

    [ShalineeBhawnani].Adding Clinique_Management

--- Clinique_Management/Clinique.py
+++ Clinique_Management/Clinique.py
@@ -3,5 +3,16 @@
 # @file  :Clinique.py
 # @author :ShalineeBhawnani
 #*******************************************************************************************************************
+import json
 class Clinique:
-    
+    def FileOpen(self):
+        with open('json_file.json') as file:
+            data=json.load(file)
+            for p in data['Doctors']:
+                print('Name: ' + p['name'])
+                print('Gender: ' + p['gender'])
+                print('Type: ' + p['type'])
+                print("Address:" +p['address'])
+                print('ContactNumber ' +p['contactNumber'])
+clinique=Clinique()
+clinique.FileOpen()
