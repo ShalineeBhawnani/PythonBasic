f4c53cf05f89ef935b0989f9e524cf56f60bbc26
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-24 14:44:20 +0530

    [Shalinee Bhawnani].Modified Commercial_Data_Processing

--- Commercial_Data_Processing/StockAccount.py
+++ Commercial_Data_Processing/StockAccount.py
@@ -18,17 +18,23 @@ if __name__ == '__main__' :
 
         except FileNotFoundError:
             print("file not found")
+        #taking user Id from user
         customerId=input("please Type Your Registerd customer Id:")  
         for data in data_key["User"]:
             if data["customerId"] == customerId:
-                print("Welcome",data["name"], "How much you want to buy shares") 
-                product_quantity=int(input("enter amount")) 
-                data["share_amount"] -= product_quantity 
+                #fetching user name based on given id
+                print("Welcome",data["name"], "How much you want to buy shares")
+                #asking product quantity
                 product_quantity=int(input("enter quantity")) 
-                data["product"]+=product_quantity   
+                data["product"]+=product_quantity  
+                #taking money from user
+                amount=int(input("enter amount")) 
+                data["share_amount"] -= amount 
+                #updating customer data
                 with open('newUser.json', '+w') as json_file:
                     json.dump(data_key, json_file)
     else:
+        #creating & appending new customer
         f = open("newUser.json","a+")
         customerId=input("Enter the customer Id: ")
         name = input("Enter the Name: ")
@@ -36,18 +42,22 @@ if __name__ == '__main__' :
         phno = int(input("Enter your Mobile Number: "))
         amount = int(input("Enter the Amount for Shares: "))
         product = int(input(" product qountity"))
-        cont = f.write(' [{'+'    "customerId" : "'+ customerId +'",\n' + '       "name" : "'+ name +'",\n' + '       "age"  : "'+str(Age)+'",\n'+'       "Ph.No" : "'+str(phno)+'",\n'+'       "share_amount" : "'+str(amount)+'"       "product"  : "'+str(product)+'",\n'+'             }]\n')
+        cont = f.write(' [{'+'    "customerId" : "'+ customerId +'",\n' + '       "name" : "'+ name +'",\n' + '       "age"  : "'+str(age)+'",\n'+'       "Ph.No" : "'+str(phno)+'",\n'+'       "share_amount" : "'+str(amount)+'"       "product"  : "'+str(product)+'",\n'+'             }]\n')
         print("The Data has been Stored Successfully in 'newUser.json' file. ")
 
 class StockAccount:
     #getting details from json file
     def getStockDetails(self):
-        file=open("/home/bridgelabz/Videos/DS/Commercial_Data_Processing/company_shares.json","r")
+        try:
+            file=open("/home/bridgelabz/Videos/DS/Commercial_Data_Processing/company_shares.json","r")
         #loading data from json
-        data_key= json.load(file)
-        data1=data_key["Company"]
-        data2=data_key["Shares"]
-        data3=data_key["Products"]
+            data_key= json.load(file)
+            data1=data_key["Company"]
+            data2=data_key["Shares"]
+            data3=data_key["Products"]
+        except FileNotFoundError:
+            print("file not found")
+            
 
         #taking input from customer to buy shares
         type=int(input("\nType:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing"))
