17162cf4f6bb6608bf73d5327d2c29f49c641cef
Shalinee <shalineebhawnani90@gmail.com>
2019-12-25 15:51:22 +0530

    [Shalinee Bhawnani].Modified Clinique_Management

--- Clinique_Management/Clinique.py
+++ Clinique_Management/Clinique.py
@@ -19,7 +19,17 @@ class Search:
             for data in data_key["Doctors"]:
                 if data["specialization"] == specialization:
                 #fetching user name based on given id
-                    print("Doctor",data["name"],data["specialization"],"specialist", "Availability Time",data["availability"])
+                    print("Doctor",data["name"],data["specialization"],"specialist", "Availability Time:",data["availability"])
+                else:
+                    f = open("new_doctor.json","a+")
+                    name = input("Enter the Name: ")
+                    id=input("Enter the doctor Id: ")
+                    specialization = input("Enter the specialization: ")
+                    contactNumber = input("Enter your Mobile Number: ")
+                    availability = input("Enter the availability time: ")
+                    cont = f.write(' [{'+'    "name" : "'+ name +'",\n' + '       "id" : "'+ id +'",\n' + '       "specialization"  : "'+str(specialization)+'",\n'+'       "contactNumber" : "'+str(contactNumber)+'",\n'+'       "availability" : "'+str(availability)+'"    }]\n')
+                    print("The Data has been Stored Successfully in 'new_doctor.json' file. ")
+
         elif(inp == "p") or (inp == "P"):
             try:
                 f = open("json_file.json","r")
@@ -33,6 +43,17 @@ class Search:
                 if data["name"] == name:
                 #fetching user name based on given id
                     print("Patient Name:",data["name"],"Patient Gender:",data["gender"], "Patient Age:",data["age"])
-         
+                else:
+                    f = open("new_patients.json","a+")
+                    name = input("Enter your Name: ")
+                    id=input("Enter the patient Id: ")
+                    profession = input("Enter the profession: ")
+                    gender = input("Enter your gender: ")
+                    age = input("Enter yor age: ")
+                    weight = input("Enter yor weight: ")
+                    height = input("Enter yor height: ")
+                    cont = f.write(' [{'+'    "name" : "'+ name +'",\n' + '       "id" : "'+ id +'",\n' + '       "gender" : "'+str(gender)+'",\n'+'       "age" : "'+str(age)+'",\n'+'       "profession"  : "'+str(profession)+'",\n'+'       "weight" : "'+weight+'",\n'+'       "height" : "'+height+'"  }]\n' )
+                    print("The Data has been Stored Successfully in 'new_patient.json' file. ")
+
 find=Search()
 find.search()
