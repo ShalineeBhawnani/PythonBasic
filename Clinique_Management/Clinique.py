85c940da7165e5b23a510484fc95ed9aee4b5588
Shalinee <shalineebhawnani90@gmail.com>
2019-12-25 14:44:12 +0530

    [Shalinee Bhawnani].Modified Clinique_Management

--- Clinique_Management/Clinique.py
+++ Clinique_Management/Clinique.py
@@ -2,33 +2,37 @@
 # @purpose :Clinique Management System using JSON.
 # @file  :Clinique.py
 # @author :ShalineeBhawnani
-#**********************************************************************************************************************
-try:
-    import json
-except ImportError:
-    print("import error")
-    
-#Executing as main program
-if __name__ == '__main__' :
-    inp = input(" Searching for doctor or Patients(d/p):")
-    try:
-        with open("json_file.json") as f:
-            data= json.load(f)
-    except FileNotFoundError:
-        print("file not found")
-# Search class  
+#**********************************************************************************************************************  
+import json
 class Search:
-    #searching for doctors & patients
-     def search(self):
-         if (inp == "d") or (inp == "D"):
-            #printing doctors details
-            for doctor in data["Doctors"]:
-                print(doctor)
+    def search(self):
+        inp = input(" whos details u want? (d/p):")
+        if (inp == "d") or (inp == "D"):
+            try:
+                f = open("json_file.json","r")
+                data_key= json.load(f)
+
+            except FileNotFoundError:
+                print("file not found")
+        #taking user Id from user
+            specialization=input("please Type Doctor specialization:")  
+            for data in data_key["Doctors"]:
+                if data["specialization"] == specialization:
+                #fetching user name based on given id
+                    print("Doctor",data["name"],data["specialization"],"specialist", "Availability Time",data["availability"])
+        elif(inp == "p") or (inp == "P"):
+            try:
+                f = open("json_file.json","r")
+                data_key= json.load(f)
+
+            except FileNotFoundError:
+                print("file not found")
+        #taking user Id from user
+            name=input("your good name:")  
+            for data in data_key["Patients"]:
+                if data["name"] == name:
+                #fetching user name based on given id
+                    print("Patient Name:",data["name"],"Patient Gender:",data["gender"], "Patient Age:",data["age"])
          
-         elif (inp=="p") or (inp=="P"):
-            #printing Patients details
-            for patient in data["Patients"]:
-                print(patient)
 find=Search()
 find.search()
-         
