cfa9cee5f2b48dc8021be729807c4e76667e81a8
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-26 17:58:11 +0530

    [Shalinee Bhawnani]Modify .modified deck_of_cards

---
+++ deck_of_cards/Deck.py
@@ -0,0 +1,111 @@
+#******************************************************************************************************************
+# @purpose :Erankuate the DeckOfCards JSON.
+# @file  :Deck.py
+# @author :ShalineeBhawnani
+#*******************************************************************************************************************
+
+# import statement
+try:
+    import random
+except ImportError:
+    print("import Error")
+
+#class card
+class Card(object):
+    # constructor
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    # Implementing build in methods so that you can print a card object
+    def __unicode__(self):
+        return self.show()
+    def __str__(self):
+        return self.show()
+    def __repr__(self):
+        return self.show()
+        
+    def show(self):
+        if self.rank == 1:
+            rank = "Ace"
+        elif self.rank == 11:
+            rank = "Jack"
+        elif self.rank == 12:
+            rank = "Queen"
+        elif self.rank == 13:
+            rank = "King"
+        else:
+            rank = self.rank
+
+        return "{} of {}".format(rank, self.suit)
+
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    # Display all cards in the deck
+    def show(self):
+        for card in self.cards:
+            print(card.show())
+
+    # Generate 52 cards
+    def build(self):
+        self.cards = []
+        for suit in ['Hearts', 'Clubs', 'Diamonds', 'Spades']:
+            for rank in range(1,14):
+                self.cards.append(Card(suit, rank))
+
+    # Shuffle the deck
+    def shuffle(self, num=1):
+        length = len(self.cards)
+        for _ in range(num):
+            # This is the fisher yates shuffle algorithm
+            for i in range(length-1, 0, -1):
+                randi = random.randint(0, i)
+                if i == randi:
+                    continue
+                self.cards[i], self.cards[randi] = self.cards[randi], self.cards[i]
+
+    # Return the top card
+    def deal(self):
+        return self.cards.pop()
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def sayHello(self):
+        print ("Hi! My name is ".format(self.name))
+        return self
+
+    # Draw n number of cards from a deck
+    # Returns true in n cards are drawn, false if less then that
+    def draw(self, deck, num=1):
+        for _ in range(num):
+            card = deck.deal()
+            if card:
+                self.hand.append(card)
+            else: 
+                return False
+        return True
+
+    # Display all the cards in the players hand
+    def showHand(self):
+        print("{}'s hand: {}".format(self.name, self.hand))
+        return self
+
+    def discard(self):
+        return self.hand.pop()
+
+myDeck = Deck()
+myDeck.shuffle()
+
+#giving player details
+name = Player("Shalinee")
+name.sayHello()
+name.draw(myDeck, 5)
+name.showHand()
