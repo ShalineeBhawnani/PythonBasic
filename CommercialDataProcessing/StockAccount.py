66f49784912411e8c7133df7418c1e89921ddf48
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-26 18:49:55 +0530

    [Shalinee Bhawnani]Modify .modified CommercialDataProcessing

---
+++ CommercialDataProcessing/StockAccount.py
@@ -0,0 +1,98 @@
+#******************************************************************************************************************
+# @purpose :Evaluate the Stock Report using JSON.
+# @file  :StockAccount.py
+# @author :ShalineeBhawnani
+#*******************************************************************************************************************
+try:
+    import json
+except ImportError:
+    print("import error")
+
+#Executing as main program
+if __name__ == '__main__' :
+    inp = input(" Are having an Existing Account? (y/n):")
+    if (inp == "y") or (inp == "Y"):
+        try:
+            f = open("newUser.json","r")
+            data_key= json.load(f)
+
+        except FileNotFoundError:
+            print("file not found")
+        #taking user Id from user
+        customerId=input("please Type Your Registerd customer Id:")  
+        for data in data_key["User"]:
+            if data["customerId"] == customerId:
+                #fetching user name based on given id
+                print("Welcome",data["name"], "How much you want to buy shares")
+                #asking product quantity
+                product_quantity=int(input("enter quantity")) 
+                data["product"]+=product_quantity  
+                #taking money from user
+                amount=int(input("enter amount")) 
+                data["share_amount"] -= amount 
+                #updating customer data
+                with open('newUser.json', '+w') as json_file:
+                    json.dump(data_key, json_file)
+    else:
+        #creating & appending new customer
+        f = open("newUser.json","a+")
+        customerId=input("Enter the customer Id: ")
+        name = input("Enter the Name: ")
+        age = int(input("Enter the Age: "))
+        phno = int(input("Enter your Mobile Number: "))
+        amount = int(input("Enter the Amount for Shares: "))
+        product = int(input(" product qountity"))
+        #writing data in json file
+        cont = f.write(' [{'+'    "customerId" : "'+ customerId +'",\n' + '       "name" : "'+ name +'",\n' + '       "age"  : "'+str(age)+'",\n'+'       "Ph.No" : "'+str(phno)+'",\n'+'       "share_amount" : "'+str(amount)+'"       "product"  : "'+str(product)+'",\n'+'             }]\n')
+        print("The Data has been Stored Successfully in 'newUser.json' file. ")
+
+class StockAccount:
+    #getting details from json file
+    def getStockDetails(self):
+        try:
+            file=open("/home/bridgelabz/Videos/DS/Commercial_Data_Processing/company_shares.json","r")
+        #loading data from json
+            data_key= json.load(file)
+            data1=data_key["Company"]
+            data2=data_key["Shares"]
+            data3=data_key["Products"]
+        except FileNotFoundError:
+            print("file not found")
+            
+
+        #taking input from customer to buy shares
+        type = int(input("\nType:0 for HCL share:Production\nType:1 for TCS share:Sales\nType 2 for CGI share: Marketing"))
+        if type == 0:
+            for data in data_key["Shares"]:
+                if data['share'] == "Production":
+                    input_amount=int(input("enter amount"))  
+                    data['amount'] += input_amount
+                    input_stock=int(input("enter stock"))  
+                    data['stock']-=input_stock
+                    with open('company_shares.json', '+w') as json_file:
+                        json.dump(data_key, json_file)
+        if type == 1:
+            for data in data_key["Shares"]:
+                print("hello")
+                if data['share'] == "Sales": 
+                    input_amount=int(input("enter amount"))
+                    data['amount'] -= input_amount
+                    with open('company_shares.json', '+w') as json_file:
+                        json.dump(data_key, json_file)
+        if type == 2:
+            for data in data_key["Shares"]:
+                print("hello")
+                if data['share'] == "Marketing":
+                    input_amount = int(input("enter amount"))
+                    data['amount'] -= input_amount
+                    with open('company_shares.json', '+w') as json_file:
+                        json.dump(data_key, json_file)
+
+        else: ("bye")
+
+#creating object
+obj1=StockAccount()
+obj2=obj1.getStockDetails()
+
+
+   
