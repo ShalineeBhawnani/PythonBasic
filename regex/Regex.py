2bcccdb8aa985eb153e8d99af0170504a6931bfd
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-26 18:00:40 +0530

    [Shalinee Bhawnani]Modify .modifid regex

---
+++ regex/Regex.py
@@ -0,0 +1,35 @@
+#******************************************************************************************************************
+# @purpose :Match Regex From String and Replace With User Details.
+# @file  :regex.py
+# @author :ShalineeBhawnani
+#*******************************************************************************************************************
+
+#importing regex module
+try:
+    import re
+except ImportError:
+    print("import error")
+
+class Regex:
+    #method to read and write file
+    def check(self):
+        with open ('file.txt', 'r') as f:
+            text = f.read()
+            print(text)
+        try:
+            #pattern matching
+            result = re.sub(r"[<]{2}[a-zA-Z]{4}[>]{2}", "<<Shalu>>", text)
+            result = re.sub(r"[<]{2}[a-zA-Z]{8}[>]{2}", "<<Shalu Bhawnani>>", result)
+            result = re.sub(r"(x){10}", "4567586948", result)
+            result = re.sub(r"X{2}/X{2}/X{4}", "01/01/2016", result)
+            file1 = open("file.txt","w")
+            file1.write(result) 
+            print(result)
+        except:
+            #printing error
+            print("An exception occurred")
+ 
+#creating object
+obj = Regex()
+#calling method 
+obj.check()
