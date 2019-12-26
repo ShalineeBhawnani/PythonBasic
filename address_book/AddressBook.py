0bb9f707fb233a3b122869b690946aaf44056396
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-26 17:54:40 +0530

    [Shalinee Bhawnani]Modify .modified address_book

---
+++ address_book/AddressBook.py
@@ -0,0 +1,17 @@
+#**********************************************************************************************************************
+# @purpose :Address Management System using JSON.
+# @file  :AddressBook.py
+# @author :ShalineeBhawnani
+#**********************************************************************************************************************
+try:
+    import json
+except ImportError:
+    print("import error")
+class AddressBook:
+    def print_address_details(self):
+        #Reading JSON from a File
+        with open('/home/bridgelabz/Videos/DS/Address_Book/address_json.json') as f:
+            #read string from JSON & storing in variable
+            data= json.load(f)
+address=AddressBook()
+address.print_address_details()
