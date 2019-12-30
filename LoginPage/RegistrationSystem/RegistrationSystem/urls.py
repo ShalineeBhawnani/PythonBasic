1fa39f62ab6b461a17f8f99d44506d7640fc2116
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-30 15:44:49 +0530

    [Shalinee Bhawnani]Refractor.refracted LoginPage

--- LoginPage/RegistrationSystem/RegistrationSystem/urls.py
+++ LoginPage/RegistrationSystem/RegistrationSystem/urls.py
@@ -1,9 +1,18 @@
+# from django.conf.urls import url
+# from django.contrib import admin
+# from loginpage.views import login
+
+# urlpatterns = [
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^login/$',login,name='login'),
+#     #url(r'^api-auth/', include('rest_framework.urls'))
+# ]
 from django.conf.urls import url
 from django.contrib import admin
-from loginpage.views import login
+from loginpage.views import login, register
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^register/$',register,name='register'),
     url(r'^login/$',login,name='login'),
-    #url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^admin/',admin.site.urls),
 ]
