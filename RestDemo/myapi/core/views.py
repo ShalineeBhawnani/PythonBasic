790f59bf9a6cf814d07409f5e77e355398292a65
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2019-12-30 17:37:33 +0530

    [ShalineeBhawnani]Adding.added RestDemo

---
+++ RestDemo/myapi/core/views.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloView(APIView):
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
