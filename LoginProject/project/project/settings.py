ef9e76d2f479585d0501ca816875f5ad2a18e3f3
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-01-04 09:20:58 +0530

    [Shalinee Bhawnani]Refractor.refracted LoginPage

--- LoginProject/project/project/settings.py
+++ LoginProject/project/project/settings.py
@@ -139,11 +139,11 @@ REST_FRAMEWORK = {
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-# EMAIL_HOST_USER = os.getenv("shalineebhawnani80@gmail.com")
-# EMAIL_HOST_PASSWORD = os.getenv("Shalinee5@5")
+EMAIL_HOST_USER = ("shalineebhawnani80@gmail.com")
+EMAIL_HOST_PASSWORD = ("Shalinee5@5")
 # EMAIL_PORT = os.getenv('EMAIL_PORT')
 
 
