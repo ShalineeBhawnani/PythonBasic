8bd5b3680e22968df38bc5200a41e7766eda69be
ShalineeBhawnani <shalineebhawnani90@gmail.com>
2020-01-11 09:42:37 +0530

    [Shalinee Bhawnani]Refractor.refracted ChatApp

---
+++ ChatApp/project/chatApp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from chatApp.models import Message, UserProfile
+
+admin.site.register(Message)
+admin.site.register(UserProfile)
---
+++ ChatApp/project/snippets/admin.py
@@ -0,0 +1,2 @@
+from django.contrib import admin
+from chatApp.models import Message
